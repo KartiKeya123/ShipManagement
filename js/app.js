13cc9910720961d1d09a77f37d48953674de5881
Kartik Thorat <kartikey.iit@live.com>
2021-09-01 19:34:00 +0530

    created welcomepage & enhaced CSS and js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const login = document.querySelector('#login');
+const signup = document.querySelector('#signup');
+const loginUrl = 'http://localhost:8080/login';
+const signupUrl = 'http://localhost:8080/adduser';
+
 const sendHttpRequest = async(method, url, data) => {
     return await fetch(url, {
         method: method,
@@ -11,12 +16,9 @@ const sendHttpRequest = async(method, url, data) => {
                 throw error;
             });
         }
-        return response.text();
+        return response;
     });
 };
-
-const signup = document.querySelector('#signup');
-const login = document.querySelector('#login');
 
 const addUser = () => {
     let email = document.getElementById("email").value;
@@ -26,7 +28,7 @@ const addUser = () => {
     let password = document.getElementById("password").value;
     let username = document.getElementById("username").value;
 
-    sendHttpRequest('POST', 'http://localhost:8080/adduser',{
+    sendHttpRequest('POST', signupUrl,{
         email: email,
         firstname: firstname,
         lastname: lastname,
@@ -34,7 +36,9 @@ const addUser = () => {
         password: password,
         username: username
     }).then(response => {
-        alert(response);
+        return response.text();
+    }).then(responseData => {
+        alert(responseData);
     }).catch(err => {
         alert(err);
     });
@@ -46,15 +50,18 @@ const loginUser = () => {
     let loginPassword = document.getElementById("login-password").value;
     let loginUsername = document.getElementById("login-username").value;
 
-    sendHttpRequest('POST', 'http://localhost:8080/login',{
+    sendHttpRequest('POST', loginUrl,{
         password: loginPassword,
         username: loginUsername
     }).then(response => {
-        alert(response);
+        return response.json();
+    }).then(responseData => {
+        alert(JSON.stringify(responseData));
     }).catch(err => {
         alert(err);
     });
+    document.getElementById('login-form').reset();
 };
 
-signup.addEventListener('click', addUser);
 login.addEventListener('click', loginUser);
+signup.addEventListener('click', addUser);
